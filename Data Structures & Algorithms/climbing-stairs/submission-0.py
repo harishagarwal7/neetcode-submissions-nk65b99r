baea7dcc62dd8eb110b832b23484a992a668f588
Harish <69117059+harishagarwal7@users.noreply.github.com>
2026-04-03 23:42:28 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.py
@@ -0,0 +1,7 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        arr=[1,2]
+        for i in range(2,n):
+            arr.append(arr[i-1]+arr[i-2])
+        return arr[n-1]
+        
